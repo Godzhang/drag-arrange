15e19a78571bd902953d3ce8b7a18289b3cf1750
godzhang <zhangqijilindaxue@126.com>
2017-10-24 12:37:39 +0800

    done drag-arrange

--- js/script.js
+++ js/script.js
@@ -19,13 +19,14 @@
 
 	var Drag = function(options){
 		this.opt = Object.assign({}, options);
-		this.container = this.opt.container;
-		this.items = this.getByClass(this.container || document, this.opt.targetClass);
-		this.dragSelector = this.opt.dragSelector;
-		this.touchDown = false;
-		this.dragging = false;
-		this.dragElement = null;
-		this.clone = null;
+		this.container = this.opt.container;     //拖动元素的容器
+		this.items = this.getByClass(this.container || document, this.opt.targetClass);  //获取元素
+		this.dragSelector = this.opt.dragSelector;										 //拖动区域
+		this.touchDown = false;															 //是否按下
+		this.dragging = false;															 //是否拖动中
+		this.dragElement = null;														 //正在拖动的元素
+		this.clone = null;																 //复制元素
+		this.dragDone = this.opt.dragDone;								                 //拖动完成后的回调
 		this.downX = 0;
 		this.downY = 0;
 		this.leftOffset = 0;
@@ -35,7 +36,9 @@
 	Drag.prototype = {
 		init: function(){
 			var self = this;
-
+			//元素必须多余1个
+			if(!this.items.length || this.items.length <= 1){return;}
+			//给每个元素或元素的拖动区域添加鼠标按下事件
 			this.items.forEach(function(cur, index){
 				if(self.dragSelector){
 					var dragArea = self.getByClass(cur, self.dragSelector);
@@ -46,10 +49,9 @@
 					self.addEvent(cur, dragEvents.START, function(event){
 						self.dragStartHandler(event, cur);
 					}, false);
-				}
-				
+				}				
 			});
-
+			//添加移动和鼠标抬起事件
 			this.addEvent(document, dragEvents.MOVE, function(event){
 				self.dragMoveHandler.call(self, event);
 			}, false);
@@ -76,18 +78,17 @@
 
 				this.clone.style.left = this.leftOffset + moveX + "px";
 				this.clone.style.top = this.topOffset + moveY + "px";
-
 				this.shiftHoveredElement(this.clone, this.dragElement, this.items);
 			}else{
 				this.clone = this.cloneNode(this.dragElement);
-
+				//在有margin值的情况下，style.left和offsetLeft相差margin的值
 				this.leftOffset = this.dragElement.offsetLeft - this.getStyle(this.dragElement, 'margin-left');
 				this.topOffset = this.dragElement.offsetTop - this.getStyle(this.dragElement, 'margin-top');
 				
 				this.clone.style.left = this.leftOffset + "px";
 				this.clone.style.top = this.topOffset + "px";
 				this.dragElement.parentNode.appendChild(this.clone);
-
+				//隐藏当前被点击的元素，拖动其复制元素
 				this.dragElement.style.visibility = 'hidden';
 				this.dragging = true;
 			}
@@ -98,26 +99,27 @@
 				this.dragging = false;
 				this.clone.parentNode.removeChild(this.clone);
 				this.dragElement.style.visibility = 'visible';
+				this.dragDone && this.dragDone.call(this.dragElement, this.dragElement, this.items);
 			}
 
 			this.touchDown = false;
 		},
 		shiftHoveredElement: function(clone, dragElement, items){
+			//获取拖动重合的元素
 			var hoveredElement = this.getHoveredElement(clone, dragElement, items);
 
-			if(hoveredElement !== dragElement){
+			if(hoveredElement && hoveredElement !== dragElement){
 				var hoveredElementIndex = this.getIndex(items, hoveredElement);  //重合元素的索引值
 				var dragElementIndex = this.getIndex(items, dragElement);        //被拖动元素的索引值
-				// console.log(hoveredElementIndex);
-				console.log(dragElementIndex);
-				// if(hoveredElementIndex < dragElementIndex){
-				// 	this.container.insertBefore(dragElement, hoveredElement);
-				// }else{
-				// 	this.container.insertBefore(dragElement, items[hoveredElementIndex+1]);
-				// }
+				//比较索引值大小，操作DOM
+				if(hoveredElementIndex < dragElementIndex && hoveredElementIndex !== -1){
+					this.container.insertBefore(dragElement, hoveredElement);
+				}else if(hoveredElementIndex > dragElementIndex && hoveredElementIndex !== -1){
+					this.container.insertBefore(dragElement, items[hoveredElementIndex+1]);
+				}
+				//同步更改items中的顺序
+				this.shifeElementPosition(items, dragElementIndex, hoveredElementIndex);
 			}
-
-			this.shifeElementPosition(items, dragElementIndex, hoveredElementIndex);
 		},
 		getHoveredElement: function(clone, dragElement, items){
 			var cloneWidth = clone.offsetWidth,
@@ -137,22 +139,22 @@
 				currentElement = items[i];
 
 				if(currentElement === dragElement) continue;
-
+				//获取除本身外每个元素的水平垂直中线的坐标值
 				horizontalMidPosition = currentElement.offsetLeft + currentElement.offsetWidth * 0.5;
 				verticalMidPosition = currentElement.offsetTop + currentElement.offsetHeight * 0.5;
-
+				//判断拖动元素是否与某个元素重合
 				overlappingX = (horizontalMidPosition < cloneRightPosition) && (horizontalMidPosition > cloneLeftPosition);
 				overlappingY = (verticalMidPosition < cloneBottomPosition) && (verticalMidPosition > cloneTopPosition);
 
 				inRange = overlappingX && overlappingY;
-
+				//重合就返回这个元素
 				if(inRange){
 					return currentElement;
 				}
 			}
 		},
 		shifeElementPosition: function(arr, fromIndex, toIndex){
-			var temp = arr.splice(fromIndex, 1);
+			var temp = arr.splice(fromIndex, 1)[0];
 			arr.splice(toIndex, 0, temp);
 		},
 		getIndex: function(arr, item){
